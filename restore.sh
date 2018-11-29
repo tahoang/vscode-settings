81a6bc3a56f141fc6aa72b751f76d988711f9db8
Tu Hoang <dhoang519@gmail.com>
2018-11-29 16:57:24 -0500

    added restoring extensions

--- restore.sh
+++ restore.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # for mac
 cp -a ./vscode/. ~/Library/Application\ Support/Code/User/.
+
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension joseroux.phoenix-dark
+code --install-extension jvandyke.vscode-circleci
+code --install-extension ms-vscode.PowerShell
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension shyykoserhiy.vscode-spotify
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension ZigaGrcar.theme-monokai-phoenix
