d98789af4514b2c4dad80ed9488b724117233878
Tu Hoang <dhoang519@gmail.com>
2018-11-29 16:58:17 -0500

    restore extensions on widows (powershell)

--- restore.ps1
+++ restore.ps1
@@ -1,2 +1,14 @@
 # restore settings 
 cp ./*.json %APPDATA%\Code\User\
+
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension joseroux.phoenix-dark
+code --install-extension jvandyke.vscode-circleci
+code --install-extension ms-vscode.PowerShell
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension shyykoserhiy.vscode-spotify
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension ZigaGrcar.theme-monokai-phoenix
